e16eb2c7e020d4d45d71f7cb29d1c89da32dde61
Rupinder <is.rupinder23@gmail.com>
2025-08-28 21:19:28 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from mangum import Mangum
 import sys
 import os
 
-# Add the parent directory to the path so we can import from the root
+# Add the parent directory to the path so we can import from the root 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
